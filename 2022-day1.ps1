adf841809c90a4d5d775ddd3be6c203139877f62
uniquegeek <uniquegeek@gmail.com>
2022-12-09 12:09:23 -0600

    Create 2022-day1.ps1

---
+++ 2022-day1.ps1
@@ -0,0 +1,23 @@
+$infile = "advent01.txt"
+$data = Get-Content $infile
+
+$production = @()
+$lines = $data.Count
+[int]$linenum = "1"
+[int]$elfnum = "1"
+[int]$calories = "0"
+
+foreach ($d in $data) {
+    if ($d) {
+        $calories = $calories + [int]$d
+    } else {
+        $production += @([pscustomobject]@{Elf=$elfnum;Calories=$calories})
+        $elfnum++
+    }
+    if ($linenum -eq $lines) {
+        $production += @([pscustomobject]@{Elf=$elfnum;Calories=$calories})
+    }
+    $linenum++
+}
+write-host "Most Calories Carried:"
+$production | Sort-Object -Property Calories -Descending | Select-Object -First 1
