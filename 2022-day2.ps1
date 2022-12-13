69869d872b700d5b6b368329426a3eb0117acce1
uniquegeek <uniquegeek@gmail.com>
2022-12-13 09:54:32 -0600

    Update 2022-day2.ps1
    code worked, but apparently I don't know how to score rock paper scissors...

--- 2022-day2.ps1
+++ 2022-day2.ps1
@@ -1,5 +1,6 @@
 #https://adventofcode.com/2022/day/2
 #uniquegeek@gmail.com
+
 $infile = "advent02.txt"
 $data = Get-Content $infile
 #input looks like:
@@ -18,23 +19,23 @@ $playtable += @([pscustomobject]@{A=1;B=2;C=3;X=1;Y=2;Z=3})
 #Win Table - 0 for loss, 3 for tie, 6 for win
 #    X Y Z
 #A   3 6 0
-#B   6 3 0
+#B   0 3 6
 #C   6 0 3
 $wintable = @()
 $wintable += @([pscustomobject]@{X=3;Y=6;Z=0})
-$wintable += @([pscustomobject]@{X=6;Y=3;Z=0})
+$wintable += @([pscustomobject]@{X=0;Y=3;Z=6})
 $wintable += @([pscustomobject]@{X=6;Y=0;Z=3})
 
-$totalwin = [int]"0"
+$totalwin = 0
 
 foreach ($d in $data) {
-    $currenthand = [int]"0"
+    $currenthand = 0
     $invalid = $false
-    [string]$them = $d[0]
-    [string]$me = $d[2]
+    [string]$them = $d[0] #first item on line
+    [string]$me = $d[2] #third item on line
     write-host $d" " -NoNewline  #show both plays from file
     if (($me_validplays.Contains($me)) -and ($them_validplays.Contains($them))) {
-        $playvalue = $playtable[0].$me  #count my card
+        $playvalue = $playtable[0].$me  #get value of my card
         #lookup their hand, and how my hand compares
         switch ($them) {
             A {$currenthand = $wintable[0].$me}
@@ -43,9 +44,9 @@ foreach ($d in $data) {
         }
         $totalwin = $totalwin + $currenthand + $playvalue
         switch ($currenthand) {
-            0 {write-host Lost! -ForegroundColor Red}
-            3 {write-host Tie. -ForegroundColor Gray}
-            6 {write-host Win!!! -ForegroundColor Green}
+            0 {write-host "Lost!" -ForegroundColor Red}
+            3 {write-host "Tie." -ForegroundColor Gray}
+            6 {write-host "Win!!!" -ForegroundColor Green}
         }
     } else {  #invalid play
         write-host "Invalid Play!" -BackgroundColor DarkGray
